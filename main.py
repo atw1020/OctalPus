691c665398c70b6f7e6534945d2ebeaa99b02cc0
Arthur Wesley <50496947+ArthurTGW@users.noreply.github.com>
2021-01-18 08:32:34 -0800

    create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+
+Author: Arthur Wesley
+
+"""
+
+import bot as b
+
+
+def main():
+    """
+
+    main method
+
+    :return:
+    """
+
+    bot = b.create_bot()
+    b.run_bot(bot)
+
+
+if __name__ == "__main__":
+    main()
